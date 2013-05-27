0c219c983535143b4f454b797b9d561ae074e4aa
xehivs <pawel@ksienie.com>
2013-05-27 16:52:28 +0200

    Zaokrąglone rogi.

--- Lokator/AppDelegate.m
+++ Lokator/AppDelegate.m
@@ -7,12 +7,15 @@
 //
 
 #import "AppDelegate.h"
+#import <QuartzCore/QuartzCore.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
+    self.window.layer.cornerRadius = 32;
+    self.window.clipsToBounds = YES;
     return YES;
 }
 							
